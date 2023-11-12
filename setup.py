3bcabd43bb4ff9a8427e476da87eb1f6dfb6b0b0
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2023-11-12 12:11:38 -0600

    Changes application name

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 import os
 from setuptools import find_packages, setup
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
-    name =              "trading-bot",
+    name =              "algo-bull",
     version =           "1.0.0",
     author =            "Gabriel C. Trahan",
     author_email =      "gabriel.trahan1@louisiana.edu",
@@ -13,7 +10,7 @@ setup(
         "Algorithmic trading automation."
     ),
     license =           "MIT",
-    url =               "https://github.com/theokoles7/Trading-Bot",
-    long_description =  read('README.md'),
+    url =               "https://github.com/theokoles7/Algo-Bull",
+    long_description =  open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     packages =          find_packages()
 )

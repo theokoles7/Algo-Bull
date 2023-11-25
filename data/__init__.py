7b15f59a159bfd186f200a26bbaa684e64b9c75e
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2023-11-25 10:48:32 -0600

    Data refactoring

---
+++ data/__init__.py
@@ -0,0 +1,6 @@
+"""Algo-Bull stock data sets."""
+
+__all__ = ['stock_univariate', 'stock_multivariate']
+
+from data.stock_univariate      import StockUnivariate
+from data.stock_multivariate    import StockMultivariate

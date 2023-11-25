888089114e9ffc5adfa6ea53f87bbc5b194a7c46
Gabriel C. Trahan <gabrieltrahan777@hotmail.com>
2023-11-25 10:48:07 -0600

    Models refactoring

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+"""Algo-Bull RNN models."""
+
+__all__ = ['lstm_univariate', 'lstm_multivariate']
+
+from models.lstm_univariate    import LSTMUnivariate
+from models.lstm_multivariate  import LSTMMultivariate
